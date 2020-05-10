c5f19024f735615566e96be681932af3b639f982
faraks01 <k.nikolaev@opinionsn.com>
2020-05-10 14:02:13 +0300

    Initial commit

---
+++ src/Layout/Header/index.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react'
+import {Layout, Menu} from "antd";
+import Logo from '../../../src/Assets/images/logo.png'
+import TextLogo from '../../../src/Assets/images/text-logo.png'
+
+import './header.scss'
+export default class Header  extends Component {
+
+    render(){
+        const {Header} = Layout;
+
+        return (
+            <Header className="flex-align-center container header" theme={'light'}>
+                <div className="logo-block">
+                    <img src={Logo} width='48px'/>
+                    <img src={TextLogo} width='120px'/>
+
+                </div>
+                <Menu theme="light" mode="horizontal" defaultSelectedKeys={['2']}>
+                    <Menu.Item key="1">Доставка</Menu.Item>
+                    <Menu.Item key="2">Контакты</Menu.Item>
+                    <Menu.Item key="3">Галерея</Menu.Item>
+                    <Menu.Item key="4">О нас</Menu.Item>
+                </Menu>
+            </Header>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import {Layout} from "antd";
+import './style.scss'
+import Header from "./Layout/Header";
+class App extends Component {
+    state = {
+      title:'hi'
+    };
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    render(){
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        return (
+           <div>
+               <Layout>
+                    <Header />
+               </Layout>
+           </div>
+        )
+    }
+
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
